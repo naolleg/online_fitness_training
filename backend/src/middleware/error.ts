f0fc64b40fd73c5c2108ef842072ffeccbb13364
naolleg <naollegesse51@gmail.com>
2024-07-04 23:05:28 +0300

    added error midleware

---
+++ backend/src/middleware/error.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from "express";
+const errorHandler:any =(method:Function)=> {
+      return async (req:Request,res:Response,next:NextFunction)=>{
+         try {
+           await method(req,res,next);
+         } catch (error:any) {
+              console.log(error.message);
+      
+         }
+      }
+   
+   }
+export default errorHandler;
