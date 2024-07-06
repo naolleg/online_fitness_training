20af0a4d3891dd0a243aa8fc7e5ae792e55a3c4f
naolleg <naollegesse51@gmail.com>
2024-07-06 23:54:00 +0300

    error handler added

---
+++ backend/src/config/errorHandler.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+import { ErrorCode, HttpExecption } from "../exception/root";
+import { InternalException } from "../exceptions/internalException.js";
+
+const errorHandler:any =(method:Function)=> {
+   return async (req:Request,res:Response,next:NextFunction)=>{
+      try {
+        await method(req,res,next);
+      } catch (error:any) {
+         //   console.log(error.message);
+         let exception:HttpExecption;
+         if(error instanceof HttpExecption){
+            exception = error;
+         }else{
+            exception = new InternalException('something is wrong',500,ErrorCode.INTERNAL_EXCEPTION,null);
+         }
+         next(exception);
+      }
+   }
+
+}
+
+export default errorHandler;
